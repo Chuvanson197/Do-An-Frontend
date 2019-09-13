c0034d786b9cb3fe4a5b4aa2350202961b7f5dba
datfc97pro <datfc97pro@gmail.com>
2019-09-13 23:14:18 +0700

    fix warning

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
-import Example from './pages/Example.page';
+import React, { lazy, Suspense } from 'react';
+import { Route, Router } from 'react-router-dom';
+import { history } from './store';
+import LoadingProgress from './components/Loading/LoadingProgress';
 
-export default [
-  {
-    component: Example,
-    exact: true,
-    path: '/'
-  }
-];
+const Example = lazy(() => import('./pages/Example.page'));
+
+const Routes = () => {
+  return (
+    <Suspense fallback={<LoadingProgress />}>
+      <Router history={history}>
+        <Route path="/" exact component={Example}></Route>
+      </Router>
+    </Suspense>
+  );
+};
+
+export default Routes;
