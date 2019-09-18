0087d79fc097d9752d941603829bda2ef4f0f635
An <husky@Huskys-Mac-mini.local>
2019-09-18 16:12:03 +0700

    project detail screen component refactor

--- src/modules/projectDetails/service.js
+++ src/modules/projectDetails/service.js
@@ -4,45 +4,46 @@ const service = {
   getProjectDetail: (body) => {
     // dummy data
     return {
-      customer_name: 'Customer A',
-      total_member: 1,
-      start_date: '2019/09/01',
-      end_date: '2019/09/30',
+      name: 'MUJI-ADMIN',
+      customer_name: 'MUJI.jp',
+      total_member: 5,
+      start_date: 1568626107000,
+      end_date: 1600248507000,
       members: [
         {
           staff_code: 'A123',
           name: 'Nguyen Van A',
           position: 'Developer',
-          effort: '0.9',
-          join_at: '2019/09/10'
+          effort: '1',
+          join_at: 1568626107000
         },
         {
           staff_code: 'A124',
           name: 'Nguyen Van A',
           position: 'Developer',
-          effort: '0.9',
-          join_at: '2019/09/10'
+          effort: '2',
+          join_at: 1568626107000
         },
         {
           staff_code: 'A125',
           name: 'Nguyen Van A',
           position: 'Developer',
-          effort: '0.9',
-          join_at: '2019/09/10'
+          effort: '3',
+          join_at: 1568626107000
         },
         {
           staff_code: 'A126',
           name: 'Nguyen Van A',
           position: 'Developer',
-          effort: '0.9',
-          join_at: '2019/09/10'
+          effort: '4',
+          join_at: 1568626107000
         },
         {
           staff_code: 'A127',
           name: 'Nguyen Van A',
           position: 'Developer',
-          effort: '0.9',
-          join_at: '2019/09/10'
+          effort: '5',
+          join_at: 1568626107000
         }
       ]
     };
