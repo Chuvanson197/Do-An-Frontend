e06e541b44026c943f4bf082a14a7fec4e84e974
An <husky@Huskys-Mac-mini.local>
2019-09-19 10:24:52 +0700

    to react-hooks <3

--- src/pages/Dashbroad.page.js
+++ src/pages/Dashbroad.page.js
@@ -1,35 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { actions } from '../modules/layout/store';
 
 import Layout from '../modules/layout/components/Layout';
 
-const propTypes = {
-  selectItem: PropTypes.func.isRequired
+const DashbroadPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(actions.selectItem(['dashboard']));
+  }, [dispatch]);
+
+  return <Layout>Dashboard</Layout>;
 };
 
-class DashbroadPage extends Component {
-  componentWillMount() {
-    const { selectItem } = this.props;
-    selectItem(['dashboard']);
-  }
-
-  render() {
-    return <Layout>Dashboard</Layout>;
-  }
-}
-
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = (dispatch) => ({
-  selectItem: (selectedKeys) => dispatch(actions.selectItem(selectedKeys))
-});
-
-DashbroadPage.propTypes = propTypes;
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(DashbroadPage);
+export default DashbroadPage;
--- src/pages/ListProject.page.js
+++ src/pages/ListProject.page.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Row, Col, Tabs, Tag } from 'antd';
 
 import { actions } from '../modules/layout/store';
@@ -11,8 +11,6 @@ import HeaderTitle from '../components/Content/HeaderTitle';
 import ListProject from '../modules/listProject/components/ListProject.container';
 
 const propTypes = {
-  selectItem: PropTypes.func.isRequired,
-
   listProject: PropTypes.arrayOf(PropTypes.shape({}))
 };
 
@@ -53,59 +51,47 @@ const defaultProps = {
   ]
 };
 
-class ListProjectPage extends Component {
-  componentWillMount() {
-    const { selectItem } = this.props;
-    selectItem(['project']);
-  }
+const ListProjectPage = ({ listProject }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(actions.selectItem(['project']));
+  }, [dispatch]);
 
-  render() {
-    const { listProject } = this.props;
-    return (
-      <Layout>
-        <React.Fragment>
-          <Row>
-            <HeaderTitle title="List of projects" />
-          </Row>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Tabs defaultActiveKey="1">
-                <Tabs.TabPane tab={<Tag color="#108ee9">Running</Tag>} key="1">
-                  <ListProject
-                    listProject={listProject.filter((item) => item.status === 'running')}
-                  />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={<Tag color="#87d068">Completed</Tag>} key="3">
-                  <ListProject
-                    listProject={listProject.filter((item) => item.status === 'completed')}
-                  />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={<Tag color="#f5222D">Stopped</Tag>} key="2">
-                  <ListProject
-                    listProject={listProject.filter((item) => item.status === 'stopped')}
-                  />
-                </Tabs.TabPane>
-              </Tabs>
-            </Col>
-            <Col span={12}></Col>
-          </Row>
-        </React.Fragment>
-      </Layout>
-    );
-  }
-}
-
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = (dispatch) => ({
-  selectItem: (selectedKeys) => dispatch(actions.selectItem(selectedKeys))
-});
+  return (
+    <Layout>
+      <React.Fragment>
+        <Row>
+          <HeaderTitle title="List of projects" />
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Tabs defaultActiveKey="1">
+              <Tabs.TabPane tab={<Tag color="#108ee9">Running</Tag>} key="1">
+                <ListProject
+                  listProject={listProject.filter((item) => item.status === 'running')}
+                />
+              </Tabs.TabPane>
+              <Tabs.TabPane tab={<Tag color="#87d068">Completed</Tag>} key="3">
+                <ListProject
+                  listProject={listProject.filter((item) => item.status === 'completed')}
+                />
+              </Tabs.TabPane>
+              <Tabs.TabPane tab={<Tag color="#f5222D">Stopped</Tag>} key="2">
+                <ListProject
+                  listProject={listProject.filter((item) => item.status === 'stopped')}
+                />
+              </Tabs.TabPane>
+            </Tabs>
+          </Col>
+          <Col span={12}></Col>
+        </Row>
+      </React.Fragment>
+    </Layout>
+  );
+};
 
 ListProjectPage.propTypes = propTypes;
 
 ListProjectPage.defaultProps = defaultProps;
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ListProjectPage);
+export default ListProjectPage;
