12b2377d521bb62d5a9517a34fe1b28b8e9cd364
Son <chuvanson321@gmail.com>
2019-10-11 09:17:33 +0700

    update member

--- src/pages/ListProject.page.js
+++ src/pages/ListProject.page.js
@@ -28,41 +28,6 @@ const styles = {
   `
 };
 
-const dummyData = [
-  {
-    id: 1,
-    name: 'MUJI-ADMIN',
-    customer: 'MUJI.jp',
-    start_time: 1568626107000,
-    end_time: 1600248507000,
-    status: 'running'
-  },
-  {
-    id: 2,
-    name: 'Project-Management',
-    customer: 'Impl.vn',
-    start_time: 1568626107000,
-    end_time: 1600248507000,
-    status: 'stopped'
-  },
-  {
-    id: 3,
-    name: 'Xaydung.co',
-    customer: 'Tekmate.co',
-    start_time: 1537256897000,
-    end_time: 1563436097000,
-    status: 'completed'
-  },
-  {
-    id: 4,
-    name: 'Epark',
-    customer: 'EPARK.jp',
-    start_time: 1568626107000,
-    end_time: 1600248507000,
-    status: 'running'
-  }
-];
-
 const ListProjectPage = ({ history }) => {
   const dispatch = useDispatch();
   const { authenticated } = useSelector((state) => state.authentication);
