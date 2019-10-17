f9ad9e352e4771a65018e22c23ef31695df97a11
nxa127 <nxa127@gmail.com>
2019-10-17 16:54:20 +0700

    add view member history function

--- src/pages/ProjectMemberHistory.page.js
+++ src/pages/ProjectMemberHistory.page.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col } from 'antd';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 import { css } from 'emotion';
 
+import { Row, Col } from 'antd';
+
 import { actions as layoutActions } from '../modules/layout/store';
-import { actions as projectActions } from '../modules/project/projectMemberHistory/store';
 
 import Layout from '../modules/layout/components/Layout';
 
@@ -27,51 +28,11 @@ const styles = {
   `
 };
 
-const dummyData = [
-  {
-    id: 'member_001',
-    staff_code: 'impl_S01',
-    name: 'Nguyễn Xuân An',
-    phonenumber: '0975352786',
-    email: 'an.nguyen@impl.vn',
-    time_in: 1568271275000,
-    time_out: 1599893675000,
-    status: 'working',
-    effort: 1,
-    position: 'Developer'
-  },
-  {
-    id: 'member_002',
-    staff_code: 'impl_S02',
-    name: 'Nguyễn Xuân An',
-    phonenumber: '0975352786',
-    email: 'an.nguyen@impl.vn',
-    time_in: 1568271275000,
-    time_out: 1599893675000,
-    status: 'out',
-    effort: 1,
-    position: 'Developer'
-  },
-  {
-    id: 'member_003',
-    staff_code: 'impl_S03',
-    name: 'Nguyễn Xuân An',
-    phonenumber: '0975352786',
-    email: 'an.nguyen@impl.vn',
-    time_in: 1568271275000,
-    time_out: 1599893675000,
-    status: 'busy',
-    effort: 1,
-    position: 'Developer'
-  }
-];
-
 const ProjectMemberHistoryPage = ({ match, history }) => {
   const dispatch = useDispatch();
   const { authenticated } = useSelector((state) => state.authentication);
   useEffect(() => {
     dispatch(layoutActions.selectItem(['project']));
-    dispatch(projectActions.getProjectMemberHistory({ path: 'project/member-history' }));
   }, [dispatch]);
 
   useEffect(() => {
@@ -88,10 +49,10 @@ const ProjectMemberHistoryPage = ({ match, history }) => {
     <Layout>
       <Row>
         <Row>
-          <HeaderTitle title="Project member history" />
+          <HeaderTitle title={<FormattedMessage id="projects.memberHistory.title" />} />
         </Row>
         <Row>
-          <ProjectMemberHistory projectMemberHistory={dummyData} />
+          <ProjectMemberHistory match={match} />
         </Row>
         <Row className={styles.footer}>
           <Col span={12}>
