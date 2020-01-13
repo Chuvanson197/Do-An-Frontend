02a740cba000cffcca6552ff3c534beeadef1b01
Test Impl <testimpl@Tests-Mac-mini.local>
2020-01-13 09:01:14 +0700

    refactor login page

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_SERVER: 'http://localhost:3000/api',
-  API_SERVER_URL: 'http://localhost:3000'
+  API_SERVER_URL: 'http://localhost:3000',
+  AUTH_URL: 'http://auth.impl.vn/oauth/authorize?client_id=15&response_type=code&scope=user-info'
 };
