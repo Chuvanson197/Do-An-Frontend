0e8305dfbb97e35c208de0149b653114a146204f
an.phan <an.phan@impl.vn>
2020-02-14 17:13:13 +0700

    a

--- src/modules/dashbroad/Gantt/Gantt.js
+++ src/modules/dashbroad/Gantt/Gantt.js
@@ -7,61 +7,6 @@ export default class Gantt extends Component {
   // instance of gantt.dataProcessor
   dataProcessor = null;
 
-  /**
-   * applies one of the predefined settings of the time scale
-   */
-  setZoom(value) {
-    switch (value) {
-      case 'Hours':
-        gantt.config.scale_unit = 'day';
-        gantt.config.date_scale = '%d %M';
-
-        gantt.config.scale_height = 60;
-        gantt.config.min_column_width = 30;
-        gantt.config.subscales = [
-          { unit: 'hour', step: 1, date: '%H' }
-        ];
-        break;
-      case 'Days':
-        gantt.config.min_column_width = 70;
-        gantt.config.scale_unit = 'week';
-        gantt.config.date_scale = '#%W';
-        gantt.config.subscales = [
-          { unit: 'day', step: 1, date: '%d %M' }
-        ];
-        gantt.config.scale_height = 60;
-        break;
-      case 'Months':
-        gantt.config.min_column_width = 70;
-        gantt.config.scale_unit = 'month';
-        gantt.config.date_scale = '%F';
-        gantt.config.scale_height = 60;
-        break;
-      default:
-        break;
-    }
-  }
-
-  initGanttDataProcessor() {
-    /**
-     * type: "task"|"link"
-     * action: "create"|"update"|"delete"
-     * item: data object object
-     */
-    const onDataUpdated = this.props.onDataUpdated;
-    this.dataProcessor = gantt.createDataProcessor((type, action, item, id) => {
-      return new Promise((resolve, reject) => {
-        if (onDataUpdated) {
-          onDataUpdated(type, action, item, id);
-        }
-
-        // if onDataUpdated changes returns a permanent id of the created item, you can return it from here so dhtmlxGantt could apply it
-        // resolve({id: databaseId});
-        return resolve();
-      });
-    });
-  }
-
   shouldComponentUpdate(nextProps) {
     return this.props.zoom !== nextProps.zoom;
   }
@@ -79,7 +24,6 @@ export default class Gantt extends Component {
       { name: "end_date", label: "End time", align: "center", width: "*" },
     ];
     gantt.init(this.ganttContainer);
-    this.initGanttDataProcessor();
     gantt.parse(tasks);
   }
 
